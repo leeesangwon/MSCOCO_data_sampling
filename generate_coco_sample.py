a4f2c19318366118c2b0e6c63380fd7c857ef90b
Sangwon Lee <leee.sangwon@gmail.com>
2018-06-21 02:11:46 +0900

    refactoring

--- generate_coco_sample.py
+++ generate_coco_sample.py
@@ -8,72 +8,97 @@ import numpy as np
 import skimage.io as io
 from pycocotools.coco import COCO
 
-dataDir = './coco'
-dataType = 'val2014'
-annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
 
-outDir = './coco_sample'
+class CocoSampler():
+    def __init__(self, data_dir, data_type, out_dir, num_sample_per_category=20, min_instance_area_ratio=0.1):
+        self.data_dir = data_dir
+        self.data_type = data_type
+        self.out_dir = out_dir
+        self.out_gt_dir = os.path.join(out_dir, 'gt')
+        self.out_img_dir = os.path.join(out_dir, 'images')
+        self.out_list_dir = os.path.join(out_dir, 'list')
+        self.num_sample_per_category = num_sample_per_category
+        self.min_instance_area_ratio = min_instance_area_ratio
+    
+    def run(self):
+        self._sample_coco()
+        self._gen_list_txt() 
+    
+    def _sample_coco(self):
+        annFile = '{}/annotations/instances_{}.json'.format(self.data_dir, self.data_type)
+        coco = COCO(annFile)
 
-coco = COCO(annFile)
+        pascal_cats = ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep', 'airplane',
+                    'bicycle', 'boat', 'bus', 'car', 'motorcycle', 'train', 'bottle', 'chair', 
+                    'dining table', 'potted plant', 'couch', 'tv']
+        # label name in pascal voc
+        #             ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep', 'aeroplane', 
+        #              'bicycle', 'boat', 'bus' ,'car', 'motorbike', 'train', 'bottle', 'chair', 
+        #              'dining table', 'potted plant', 'sofa', 'tv/monitor']
 
-cats = coco.loadCats(coco.getCatIds())
+        for cat in pascal_cats:
+            out_img_folder = os.path.join(self.out_img_dir, cat)
+            out_gt_folder = os.path.join(self.out_gt_dir, cat)
 
-pascal_cats = ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep', 'airplane',
-               'bicycle', 'boat', 'bus', 'car', 'motorcycle', 'train', 'bottle', 'chair', 
-               'dining table', 'potted plant', 'couch', 'tv']
-# label name in pascal voc
-#             ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep', 'aeroplane', 
-#              'bicycle', 'boat', 'bus' ,'car', 'motorbike', 'train', 'bottle', 'chair', 
-#              'dining table', 'potted plant', 'sofa', 'tv/monitor']
+            try:
+                os.makedirs(out_img_folder)
+                os.makedirs(out_gt_folder)
+            except FileExistsError:
+                pass
 
-for cat in pascal_cats:
-    out_img_folder = os.path.join(outDir, 'images', cat)
-    out_gt_folder = os.path.join(outDir, 'gt', cat)
+            catIds = coco.getCatIds(catNms=[cat])
+            imgIds = coco.getImgIds(catIds=catIds)
+            imgs = coco.loadImgs(imgIds)
+            random.shuffle(imgs)
+            num_saved = 0
+            for img in imgs:
+                img_area = img['height'] * img['width']
+                min_area = img_area * self.min_instance_area_ratio
+                annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, areaRng=[min_area, img_area],iscrowd=None)
+                anns = coco.loadAnns(annIds)
+                if anns:
+                    random_ann = random.choice(anns)
+                    mask = coco.annToMask(random_ann) # 0 or 1
+                    mask *= 255
+                    mask = Image.fromarray(mask)
 
-    try:
-        os.makedirs(out_img_folder)
-        os.makedirs(out_gt_folder)
-    except FileExistsError:
-        pass
+                    img_path = os.path.join(self.data_dir, 'images', self.data_type, img['file_name'])
+                    out_img_path = os.path.join(out_img_folder, img['file_name'])
+                    out_gt_path = os.path.join(out_gt_folder, img['file_name'].split('.')[0] + '.png')
+                    shutil.copy2(img_path, out_img_path)
+                    mask.save(out_gt_path)
+                    num_saved += 1
+                
+                if num_saved == self.num_sample_per_category:
+                    break
 
-    catIds = coco.getCatIds(catNms=[cat])
-    imgIds = coco.getImgIds(catIds=catIds)
-    imgs = coco.loadImgs(imgIds)
-    random.shuffle(imgs)
-    num_saved = 0
-    for img in imgs:
-        min_ratio = 0.1
-        img_area = img['height'] * img['width']
-        min_area = img_area * min_ratio
-        annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, areaRng=[min_area, img_area],iscrowd=None)
-        anns = coco.loadAnns(annIds)
-        if anns:
-            random_ann = random.choice(anns)
-            mask = coco.annToMask(random_ann)
-            mask *= 255
-            mask = Image.fromarray(mask)
+    def _gen_list_txt(self):
+        img_list = glob.glob(os.path.join(self.out_img_dir, '*/*'))
+        gt_list = glob.glob(os.path.join(self.out_gt_dir, '*/*'))
 
-            img_path = os.path.join(dataDir, 'images', dataType, img['file_name'])
-            out_img_path = os.path.join(out_img_folder, img['file_name'])
-            out_gt_path = os.path.join(out_gt_folder, img['file_name'].split('.')[0] + '.png')
-            shutil.copy2(img_path, out_img_path)
-            mask.save(out_gt_path)
-            num_saved += 1
+        try:
+            os.makedirs(self.out_list_dir)
+        except FileExistsError:
+            pass
         
-        if num_saved == 20:
-            break
+        out_path = os.path.join(self.out_list_dir, "coco.txt")
+        
+        with open(out_path, 'w') as f:
+            for i, g in zip(img_list, gt_list):
+                i = i.replace(self.out_dir + '/', '').replace('\\', '/')
+                g = g.replace(self.out_dir + '/', '').replace('\\', '/')
+                print(i, g, file=f)
 
-img_list = glob.glob(os.path.join(outDir, 'images/*/*'))
-gt_list = glob.glob(os.path.join(outDir, 'gt/*/*'))
 
-out_list_path = os.path.join(outDir, 'list')
-try:
-    os.makedirs(out_list_path)
-except FileExistsError:
-    pass
+def main():    
+    coco_sampler = CocoSampler(data_dir = './coco',
+                               data_type = 'val2014',
+                               out_dir = './coco_sample',
+                               num_sample_per_category=20, 
+                               min_instance_area_ratio=0.1,
+                               )
+    coco_sampler.run()
 
-with open(os.path.join(out_list_path, "coco.txt"), 'w') as f:
-    for i, g in zip(img_list, gt_list):
-        i = i.replace(outDir + '/', '').replace('\\', '/')
-        g = g.replace(outDir + '/', '').replace('\\', '/')
-        print(i, g, file=f)
+
+if __name__ == '__main__':
+    main()
